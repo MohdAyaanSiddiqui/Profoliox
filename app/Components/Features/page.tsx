63de62b021cf0103ac08013215c7fcc50dd3eab2
Mohd Ayaan Siddiqui <muhammadxayaan007@gmail.com>
2026-03-11 14:42:26 +0530

    ,,,

--- app/Components/Features/page.tsx
+++ app/Components/Features/page.tsx
@@ -1,7 +1,13 @@
 'use client'
 import React from "react";
+
+type Feature = {
+    icon:React.ReactNode,
+    title:string,
+    description:string
+}
 const Feature = () => {
-    const featuresData = [
+    const featuresData: Feature[] = [
         {
             icon: <svg className='text-white' xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M20 13c0 5-3.5 7.5-7.66 8.95a1 1 0 0 1-.67-.01C7.5 20.5 4 18 4 13V6a1 1 0 0 1 1-1c2 0 4.5-1.2 6.24-2.72a1.17 1.17 0 0 1 1.52 0C14.51 3.81 17 5 19 5a1 1 0 0 1 1 1z" /><path d="m9 12 2 2 4-4" /></svg>,
             title: "Intelligent Workflow Automation",
@@ -49,7 +55,7 @@ const Feature = () => {
                 <h2 className="text-3xl md:text-[40px]/12 font-medium text-gray-100 max-w-lg text-center leading-tight">AI Agents That Automate and Accelerate Growth.</h2>
                 <p className='text-base/7 text-gray-200 max-w-xl text-center'>Streamline operations, boost productivity, and scale effortlessly - all powered by intelligent automation.</p>
                 <div className="relative max-w-6xl mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-6">
-                    {featuresData.map((feature, index) => (
+                    {featuresData.map((feature:Feature, index:number) => (
                         <div key={index} className='bg-linear-to-b from-[#020204] to-[#191130] border border-gray-700 rounded-lg p-6 space-y-3 hover:-translate-y-1 transition duration-300'>
                             {feature.icon}
                             <p className='font-medium text-lg text-gray-100'>{feature.title}</p>
